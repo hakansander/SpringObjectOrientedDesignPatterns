ea2dfb798b45206aaf39a80a82d7e0efb05d2008
Hakan Sander <hakansander@gmail.com>
2020-11-24 00:10:40 +0300

    Add adapter pattern

---
+++ 2-StructuralDesignPatterns/1-Adapter/src/main/java/com/hakansander/designpatternsapp/adapter/AdvancedMediaPlayer.java
@@ -0,0 +1,6 @@
+package com.hakansander.designpatternsapp.adapter;
+
+public interface AdvancedMediaPlayer {
+    void playVlc(String fileName);
+    void playMp4(String fileName);
+}
---
+++ 2-StructuralDesignPatterns/1-Adapter/src/main/java/com/hakansander/designpatternsapp/adapter/MediaPlayer.java
@@ -0,0 +1,5 @@
+package com.hakansander.designpatternsapp.adapter;
+
+public interface MediaPlayer {
+    void play(String audioType, String fileName);
+}
